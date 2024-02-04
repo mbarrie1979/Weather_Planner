d4d0cb330635a605eab970c15c9f75c3a662f464
Mark Barrie <mark.a.barrie@gmail.com>
2024-02-04 16:26:11 -0500

    user input now displays as current city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,41 @@
 console.log("HI!")
 
-
-var city = 'Orlando';
+var userInput = $('#text-input')
+var userCities = [];
+var currentCity = $('#current-city');
+var currentWeatherList = $('#current-weather')
+var city;
 var weatherAPIKey = 'f5ae2638dc599c5d3619396cd657ae93';
-var requestWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + weatherAPIKey + '&units=imperial';
+
 var weather = {};
-var currentCityInfo = {
-    city: "",
-    temperature: "",
-    wind: "",
-    humidity: ""
-}
+
+userInput.on('change', function () {
+    city = this.value
+    getWeather()
+    currentCity.text(city);
+})
 
 
 // rewuest for openWeather API
-$.ajax({
-    url: requestWeatherUrl,
-    method: 'GET',
-    success: function (response) {
-        // Assuming the request was successful and data is retrieved
-        weather.temp = response.main.temp;
-        weather.feels_like = response.main.feels_like;
-        weather.description = response.weather[0].description;
-        weather.wind_speed = response.wind.speed;
-        weather.humidity = response.main.humidity;
-        console.log(weather);
-    },
-    // if invalid city is entered
-    error: function (xhr, status, error) {
-        // Handle error scenario, such as when the city is not found
-        alert("Please enter a valid city. Error: " + error);
-    }
-});
+function getWeather() {
+    var requestWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + weatherAPIKey + '&units=imperial';
+    $.ajax({
+        url: requestWeatherUrl,
+        method: 'GET',
+        success: function (response) {
+            // Assuming the request was successful and data is retrieved
+            weather.temp = response.main.temp;
+            weather.wind_speed = response.wind.speed;
+            weather.humidity = response.main.humidity;
+            console.log(weather);
+        },
+        // if invalid city is entered
+        error: function (xhr, status, error) {
+            // Handle error scenario, such as when the city is not found
+            // alert("Please enter a valid city. Error: " + error);
+        }
+    });
+}
 
 
 
